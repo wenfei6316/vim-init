fb2ddcfef74fb5f88b74f58bd8ac5800c85d67a6
ckelsel <ckelsel@hotmail.com>
2019-08-01 16:30:12 +0800

    auto build vim8 for ubuntu

--- build_vim.sh
+++ build_vim.sh
@@ -1,13 +1,15 @@
 #!/bin/sh
 
-awk -F= '/^NAME/{print $2}' /etc/os-release
-
 # ubuntu 14.04
+sudo apt-get install lua5.1  liblua5.1-0-dev luajit libluajit-5.1 python3.4-dev zsh python3-pip
+sudo pip3 install typing
+
 # sudo apt-get install lua5.1  liblua5.1-0-dev luajit libluajit-5.1
 # sudo apt-get install python3.4-dev zsh
 #
 # sudo apt-get install python3-pip
 # sudo pip3 install typing
+#------------------------
 
 # Fedora
 # sudo dnf install -y ruby ruby-devel lua lua-devel luajit \
